86780c55c9664414d1a325132881e2c9f7a15ce0
dillonzellis <dillonzellis@gmail.com>
2023-07-03 11:26:47 -0400

    update loan rates

--- app/loans/loanData.ts
+++ app/loans/loanData.ts
@@ -8,27 +8,27 @@ export const Loans = [
   {
     accountType: "Current & Prior Year Vehicle",
     term: "24 - 84* Months",
-    apr: "3.50% - 18.00%",
+    apr: "4.75% - 18.00%",
   },
   {
     accountType: "2021-2020 Used Vehicle",
     term: "24 - 84* Months",
-    apr: "4.50% - 18.00%",
+    apr: "5.00% - 18.00%",
   },
   {
     accountType: "2019-2016 Used Vehicle",
     term: "24 - 84* Months",
-    apr: "5.50% - 18.00%",
+    apr: "6.00% - 18.00%",
   },
   {
     accountType: "Current & Prior Year RV/Boat/Motorcycle/Camper",
     term: "24 - 84* Months",
-    apr: "4.50% - 18.00%",
+    apr: "5.50% - 18.00%",
   },
   {
     accountType: "2021-2016 Used RV/Boat/Motorcycle/Camper",
     term: "24 - 84* Months",
-    apr: "5.50% - 18.00%",
+    apr: "6.50% - 18.00%",
   },
   {
     accountType: "Personal/Signature Loan",
--- app/loans/page.tsx
+++ app/loans/page.tsx
@@ -29,7 +29,7 @@ const LoanTable = () => {
           ))}
           <Disclaimer
             disclaimer="The above rates include 3 tiers based on the applicants credit worthiness and amount of down payment, if any. The maximum annual percentage rate will never be greater than 18.00%. The Annual Percentage Rate, corresponding daily periodic rates, late payment charges, amount and due date of payments, and other charges that may be imposed will be determined at the time of each advance and disclosed on the Advance Request Voucher.  *72 month contract available for loans over $25,000 - 84 month contract available for loans over $30,000"
-            date="*APR is accurate as of January 9, 2023."
+            date="*APR is accurate as of June 26, 2023."
           />
         </RateTable>
       </TableContainer>
